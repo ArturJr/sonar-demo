562d493beb1813ed798c8ec422b2ac949dd60e20
Artur Junior <arturjr.silva@gmail.com>
2020-09-28 23:39:37 -0300

    Add Projeto

---
+++ src/main/java/br/springboot/spring/sonar/SpringSonarApplication.java
@@ -0,0 +1,13 @@
+package br.springboot.spring.sonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSonarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSonarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/springboot/spring/sonar/SpringSonarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.springboot.spring.sonar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSonarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
